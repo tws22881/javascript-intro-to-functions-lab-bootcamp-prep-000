bb3b93070808b830b3499937523269574d49386e
Trevor Sisk <trevorsisk228@gmail.com>
2017-09-14 01:16:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 function shout(string){
 
   return string.toUpperCase();
-  
+
 }
 
 function whisper(string){
 
   return string.toLowerCase();
-  
+
 }
 function logShout(string){
 
   console.log(string.toUpperCase());
-  
+
 }
 function logWhisper(string){
 
 console.log(string.toUpperCase());
-  
+
 }
 function sayHiToGrandma(string){
 
@@ -25,11 +25,10 @@ function sayHiToGrandma(string){
       return "I can't hear you!";
   }
   if(string === string.toUpperCase()){
-      return "YES INDEED!";    
+      return "YES INDEED!";
   }
   if(string === "I Love you, Grandma."){
     return "I love you, too.";
   }
-  
-}
 
+}
